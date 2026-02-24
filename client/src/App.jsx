133ca1c581d166e4fd03f3cc61522da098addc74
Rushigayake03 <119030444+Rushigayake03@users.noreply.github.com>
2026-02-24 18:58:47 +0530

    Initial QuickDrop setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
